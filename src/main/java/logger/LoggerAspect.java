6bb716564d8c5fe7a278328e4e878e5af4c4c2fe
Толчанов Евгений <tolchanove@mail.ru>
2022-01-18 16:48:12 +0300

    lesson_2 - Аспектно-ориентированное программирование в Spring

---
+++ src/main/java/logger/LoggerAspect.java
@@ -0,0 +1,39 @@
+package logger;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+
+public class LoggerAspect {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("@annotation(LoggerExecutor)")
+    public void logsPointCut() {	}
+
+    @Before("logsPointCut()")
+    public void logBefore(JoinPoint joinPoint) {
+        if(logger.isInfoEnabled())
+            logger.info("\nBefore \nMethod <"+joinPoint.getSignature().getName() + "> with args: " + joinPoint.getArgs()[0]);
+    }
+
+    @After("logsPointCut()")
+    public void logAfter(JoinPoint joinPoint) {
+        if(logger.isInfoEnabled())
+            logger.info("\nAfter \nMethod <"+joinPoint.getSignature().getName() + ">");
+    }
+
+    @AfterReturning(pointcut = "logsPointCut()", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        if(logger.isInfoEnabled())
+            logger.info("\nAfterReturning \nMethod <"+joinPoint.getSignature().getName() + "> return: " + result.toString());
+    }
+
+}
+
